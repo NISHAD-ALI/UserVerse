5b7d0b8e2ccd045c70566af16f8f36fc7b3f174e
Nishad Ali <139544048+NISHAD-ALI@users.noreply.github.com>
2023-09-24 16:23:44 +0530

    Add files via upload

---
+++ routes/mongodb.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const { emit } = require('../app');
+
+mongoose.connect('mongodb://127.0.0.1:27017/new')
+.then(()=>{
+    console.log('mongoDb connected');
+})
+.catch(()=>{
+    console.log('failed to connect');
+})
+
+const logInSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const collection = new mongoose.model('users',logInSchema)
+
+module.exports= collection
